e44b13e01bcaa7f36438d72d1610b2ebb5a7bc8a
Raghav0111 <raghavkaushal.1999@gmail.com>
2021-03-17 21:22:20 +0530

    Height OF BT

--- Tree/Height_of_binary_Tree.cpp
+++ Tree/Height_of_binary_Tree.cpp
@@ -14,3 +14,31 @@ int FindHeight(node* root){
     }
     return max(FindHeight(root->left),FindHeight(root->right)) +1 ;
 }
+
+node* Insert(node*root,int value){
+    if (root == NULL){
+        root = new node();
+        root->data = value;
+        root->left = root->right = NULL;
+    }
+    else if (value<= root->data){
+        root->left = Insert(root->left,value);
+    }
+    else{
+        root->right = Insert(root->right,value);
+    }
+    return root;
+}
+
+int main(){
+    node*root =  NULL;
+    root = Insert(root,15);
+    root = Insert(root,10);
+    root = Insert(root,20);
+    root = Insert(root,25);
+    root = Insert(root,8);
+    root = Insert(root,12);
+
+    cout<<FindHeight(root)<<endl;;
+
+}

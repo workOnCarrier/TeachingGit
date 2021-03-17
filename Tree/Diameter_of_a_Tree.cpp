0df9547ad57500e3e7441223ee023e4e433d8eb0
Raghav0111 <raghavkaushal.1999@gmail.com>
2021-03-18 03:37:32 +0530

    Incomplete

---
+++ Tree/Diameter_of_a_Tree.cpp
@@ -0,0 +1,22 @@
+// Longest Path Between last 2 Nodes of a Tree.
+
+#include<iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node *left, *right;
+};
+
+int LeftTree(){};
+int parsingRightTree(){};
+
+Node* Diameter(Node* root){
+    if(root == NULL){
+        return;
+    }
+    while(root!=NULL){
+
+    }
+    
+}

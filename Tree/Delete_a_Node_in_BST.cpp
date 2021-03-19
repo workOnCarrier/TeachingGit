b50c91c62f66c291bf1a863c95e4219b71a9294d
Raghav0111 <raghavkaushal.1999@gmail.com>
2021-03-20 04:46:53 +0530

    Deletion of a node

--- Tree/Delete_a_Node_in_BST.cpp
+++ Tree/Delete_a_Node_in_BST.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<climits>
 using namespace std;
 
 struct Node{
@@ -29,22 +30,65 @@ Node* Insert(Node * root,int data){
 
 }
 
-bool BinarySearch(Node*root, int key){
-    if (root == NULL){
-        return false;
-    }
+// bool BinarySearch(Node*root, int key){
+//     if (root == NULL){
+//         return false;
+//     }
     
-    else if(root->data == key ){
-        return true;
-    }
-    else if(root->data>key){
-        return BinarySearch(root->left,key);
-    }
-    else if(key>root->data){
-        return BinarySearch(root->right,key);
+//     else if(root->data == key ){
+//         return true;
+//     }
+//     else if(root->data>key){
+//         return BinarySearch(root->left,key);
+//     }
+//     else if(key>root->data){
+//         return BinarySearch(root->right,key);
+//     }
+// }
+Node* FindMin(Node* root){
+    while(root->left!=NULL) FindMin(root->left);
+    return root;
+};
+Node* DeleteNode(Node*root , int data){
+    if(root == NULL) return root;
+    else if(data < root->data) root->left = DeleteNode(root->left,data);
+    else if(data > root->data) root->right = DeleteNode(root->right,data);
+
+    else{
+        if(root->left == NULL && root->right == NULL){
+                delete root;
+                root = NULL;
+                return root;
+        }
+        else if (root->left == NULL){
+            Node* temp = root;
+            root = root->right;
+            delete temp;
+            return root;
+        }
+        else if (root->right == NULL){
+            Node* temp = root;
+            root = root->left;
+            delete temp;
+            return root;
+        }
+        else{
+            Node* temp = FindMin(root->right);
+            root->data = temp->data;
+            root->right = DeleteNode(root->right,temp->data);
+        }
     }
 }
 
+
+
+void InOrder(Node* root){
+    if(root == NULL) return;
+    
+    InOrder(root->left);
+    cout<<root->data<<" ";
+    InOrder(root->right);
+}
 
 int main(){
     Node* root = NULL;
@@ -54,13 +98,17 @@ int main(){
     root = Insert(root,25);
     root = Insert(root,8);
     root = Insert(root,12);
+    cout<<"Previously tree was like: ";
+    InOrder(root);
+    
+    cout<<"\nEnter the Number you want to Delete: ";
+    int number;
+    cin>>number;
 
-    if (BinarySearch(root,10)== true){
-        cout<<"Found It \n";
-    }
-    else{
-        cout<<"The key Doesnt Exist \n";
-    }
+    DeleteNode(root,number);
+    cout<<"\nAfter Deletion its like: ";
+    InOrder(root);
+
     return 0;
 
 }

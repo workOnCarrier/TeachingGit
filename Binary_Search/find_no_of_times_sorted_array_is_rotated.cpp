65739fb791fd6907fd951503e6fbba3c68a5be36
Raghav0111 <raghavkaushal.1999@gmail.com>
2021-04-30 06:11:07 +0530

    Number of times Array is Rotated

--- Binary_Search/find_no_of_times_sorted_array_is_rotated.cpp
+++ Binary_Search/find_no_of_times_sorted_array_is_rotated.cpp
@@ -3,27 +3,25 @@ using namespace std;
 
 int times_rotated(int arr[],int n){
 
-    int start = arr[0];
+    int start = 0;
     int end = n-1;
     int ans  = -1;
     while(start<=end){
         if(arr[start]<= arr[end]){
             return start;
         }
-        else{
-            int mid  = (start+end)/2;
-            int next = (mid+1)%n;
-            int prev = ((mid+n) -1)%n;
+        int mid  = (start+end)/2;
+        int next = (mid+1)%n;
+        int prev = ((mid+n) -1)%n;
 
-            if(arr[mid]<=arr[prev] && arr[mid]<= arr[next]) {
-                ans =  mid;
-            }
-            else if(arr[mid]>=arr[start]){
-                start  = mid+1;
-            }
-            else{
-                end  = mid-1;
-            }
+        if(arr[mid]<=arr[prev] && arr[mid]<= arr[next]) {
+            ans =  mid;
+        }
+        else if(arr[mid]>=arr[start]){
+            start  = mid+1;
+        }
+        else{
+            end  = mid-1;
         }    
     }
     return ans;

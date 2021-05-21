9fc96a91f34b58ee7040820d5d1fa9f3260908a7
Raghav0111 <raghavkaushal.1999@gmail.com>
2021-05-22 05:00:00 +0530

    Cycle Detection in Undirected Graph

---
+++ Graphs/Cycle_Detection_in_uD_Graph.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool iscycle(int src,vector<vector<int>> &adj, vector<bool> &vis, int parent){
+    vis[src] = true;
+    for(auto i : adj[src]){
+        if(i!= parent){
+            if(vis[i]){
+                return true;
+            }
+            if(!vis[i] and iscycle(i,adj,vis,src)){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+int main(){
+
+    int n,m; cin>>n>>m;
+    vector<vector<int>>adj(n);
+
+    for(int i=0;i<m;i++){
+        int x,y;
+        cin>>x>>y;
+
+        adj[x].push_back(y);
+        adj[y].push_back(x);
+    }
+
+    bool cycle = false;
+    vector<bool> vis(n,false);
+
+    for(int i=0;i<n;i++){
+        if(!vis[i] and iscycle(i,adj,vis,-1)){
+            cycle  = true;  
+        }
+    }
+    if(cycle){
+        cout<<"Cycle is Present"<<endl;
+    }
+    else {
+        cout<<"Cycle is not present"<<endl;
+    }
+    
+
+    return 0;
+}
